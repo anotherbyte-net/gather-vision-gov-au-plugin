f8279c25c3afc75423b550304656ef4abbaba224
Mark Cottman-Fields <cofiem@gmail.com>
2022-09-23 23:48:25 +1000

    docs: generate docs

---
+++ docs/index.js
@@ -0,0 +1,33 @@
+URLS=[
+"gather_vision_gov_au_plugin/index.html",
+"gather_vision_gov_au_plugin/plugin.html"
+];
+INDEX=[
+{
+"ref":"gather_vision_gov_au_plugin",
+"url":0,
+"doc":""
+},
+{
+"ref":"gather_vision_gov_au_plugin.plugin",
+"url":1,
+"doc":""
+},
+{
+"ref":"gather_vision_gov_au_plugin.plugin.PluginEntry",
+"url":1,
+"doc":"The entry class for the plugin."
+},
+{
+"ref":"gather_vision_gov_au_plugin.plugin.PluginEntry.update",
+"url":1,
+"doc":"Run the update action. Args: args: The arguments for update. Returns: The result of the update action.",
+"func":1
+},
+{
+"ref":"gather_vision_gov_au_plugin.plugin.PluginEntry.show",
+"url":1,
+"doc":"Run the show action. Args: args: The arguments for show. Returns: The result of the show action.",
+"func":1
+}
+]
